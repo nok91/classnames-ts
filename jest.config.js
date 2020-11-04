be573b2674a71d92ac2a9cdab2754750fecd1d21
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-11-04 12:33:37 +0000

    🔧 configuration Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    roots: [
+        "<rootDir>/src",
+        "<rootDir>/tests"
+    ],
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+      ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    collectCoverageFrom: [
+        "./**/*.{js,jsx,ts,tsx}",
+        "!<rootDir>/node_modules/"
+    ],
+    coverageThreshold: {
+        "global": {
+          "lines": 90,
+          "statements": 90
+        }
+    }
 }
